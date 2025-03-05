747ef897d8a6e7a9087819c371781544c5cb4d83
Dika2408 <141701838+Dika2408@users.noreply.github.com>
2025-03-05 08:57:10 +0700

    Add files via upload

---
+++ koneksi/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+
+  $koneksi = new mysqli("localhost","penp6843_tamu_user","p3nm4dk0t4t451k","penp6843_tamu_new");
+  if ($koneksi->connect_error) {
+  die("Connection failed: " . $koneksi->connect_error);
+}
+
+ ?>
